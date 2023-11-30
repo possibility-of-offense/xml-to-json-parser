ccbb4142ae2b1e0f1a5ee9a90df6c1984fac6db3
Ventsislav Iliev <ventsislav.iliev@zautre.com>
2023-12-01 00:42:23 +0200

    Delete unnecessary helper files

--- src/helpers/build_variants_dictionaries.py
+++
@@ -1,2 +0,0 @@
-# This will build the variants dictionaries, if the product has variations based on the variations values
-def build_variants_dictionaries():
--- src/helpers/merge_dictionaries.py
+++ src/helpers/merge_dictionaries.py
@@ -4,9 +4,9 @@ def merge_dictionaries(current_dictionary, original_dictionary, id):
         current_dictionary is the dictionary at the which the loop is just iterating
         original_dictionary is the dictionary already exists in products_info and to be merged with
     """
+
     for i, v in current_dictionary[id].items():
         if i not in original_dictionary[id]:
             original_dictionary[id][i] = v
         else:
-            print(i, v, original_dictionary[id])
-            original_dictionary[id][i].append(v)
+            original_dictionary[id][i].extend(v)
